b761f7c260150d23bacb452ac64537d0f40fbfa9
Cesar Whesly Segura <cesarw.segura@gmail.com>
2020-08-23 10:48:39 -0500

    0

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socket
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 6790               # Arbitrary non-privileged port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
     conn, addr = s.accept()
